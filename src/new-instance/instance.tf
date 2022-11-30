ecddae168a04bf2b5a4c4ee53628bcf7a2f1b36c
Debbie <56561804+debbie-villanueva@users.noreply.github.com>
2022-11-30 14:08:56 +0100

    provider is removed to separate file

--- src/new-instance/instance.tf
+++ src/new-instance/instance.tf
@@ -1,12 +1,4 @@
-provider "aws" {
-  access_key = "HRPHYZTSEY7FKRJSV2WW" 
-  secret_key = "K+Ql26+9HCitmwAjvO317VvfqmJKbnX7VrCfXg5g"
-  region     = "eu-west-1"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0f29c8402f8cce65c"
   instance_type = "t2.micro"
 }
-
-# pd: access & secret keys are made up but with the same string length both should have
