7dfb09cf032be19e9f772f4b1f89ec6671aa035f
Debbie <56561804+debbie-villanueva@users.noreply.github.com>
2022-11-30 13:49:52 +0100

    Update main.tf

--- src/variables/main.tf
+++ src/variables/main.tf
@@ -10,17 +10,26 @@ variable "mymap"{
   }
 }
 
+# A list is always ordered, it'll always return 1,2,3 and not 3,2,1 
+# despite the order of insertion
 variable "mylist"{
   type = list
   default = [1,2,3]
 }
 
-variable "a-number"{
+# A set contains unique values, consecuently [5,1,1,2] becomes [1,2,5],
+#  it doesn`t keep the order but terraform will sort it
+variable "myset"{
+  type = set
+  default = [1,2,3]
+}
+
+variable "mynumber"{
   type = number
   default = 3874
 }
 
-variable "a-boolean"{
+variable "myboolean"{
   type = bool
   default = true
 }
