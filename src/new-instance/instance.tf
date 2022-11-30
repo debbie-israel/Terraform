3cda98cab2b37a37cb629132e569b1b404e8f4a5
Debbie <56561804+debbie-villanueva@users.noreply.github.com>
2022-11-30 14:10:27 +0100

    Update instance.tf

--- src/new-instance/instance.tf
+++ src/new-instance/instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0f29c8402f8cce65c"
+  # ami           = "ami-0f29c8402f8cce65c"
+  ami           = ${lookup(var.AMIS, var.AWS_REGION)}
   instance_type = "t2.micro"
 }
