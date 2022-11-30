f7f73f5e6ced1203392852fc7d83aca61ceabcee
Deborah Villanueva <56561804+debbie-villanueva@users.noreply.github.com>
2022-11-30 11:14:30 +0100

    Update resource.tf

--- src/aws/resource.tf
+++ src/aws/resource.tf
@@ -6,6 +6,13 @@ variable "AWS_REGION"{
   type  = string
 }
 
+variable "AMIS"{
+  type = map(string)
+  default = {
+    eu-west-1 = "my ami" 
+  }
+}
+
 resource "aws_instance" "example"{
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
